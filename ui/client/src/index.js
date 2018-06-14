28f47b2d392c9155c5fe0944c23482f2af13cd70
Georgy Georgiev <g.georgiev@relay42.com>
2018-06-14 17:27:44 +0300

    remove index.css import

--- ui/client/src/index.js
+++ ui/client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import './index.css';
 
 import App from './App';
 import store from "./js/store/index";
